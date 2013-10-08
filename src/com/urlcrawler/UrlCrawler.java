ac11039093280d61b0445bcda38bf70a89464d36
Yaroslav Karandashev <yarix7@gmail.com>
2013-10-08 23:43:56 +0400

    Add reading configuration from property file with demo configuration

--- src/com/urlcrawler/UrlCrawler.java
+++ src/com/urlcrawler/UrlCrawler.java
@@ -1,24 +1,40 @@
 package com.urlcrawler;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.Properties;
 import java.util.Queue;
 
 public class UrlCrawler {
-
+    private final static String CONFIG_FILE = "config.properties";
     private static Integer urlLimit;
-    private static Integer counter;
+    private static Integer counter = 1;
     private static URL startUrl;
-    private static Queue<URL> todoUrls;
+    private static Queue<URL> todoUrls = new LinkedList<URL>();
     private static UrlFetcher urlFetcher;
     private static LineParserImpl lineParser;
+    private static String urlRegEx;
 
     public static void main(String[] args) throws Exception {
+        initConfiguration();
         todoUrls.add(startUrl);
         lineParser = new LineParserImpl();
+        lineParser.setRegularExpression(urlRegEx);
 
         initFetcher();
         startLoop();
+    }
+
+    private static void initConfiguration() throws FileNotFoundException,
+            IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(CONFIG_FILE));
+        startUrl = new URL(properties.getProperty("startUrl"));
+        urlLimit = Integer.parseInt(properties.getProperty("urlLimit"));
+        urlRegEx = properties.getProperty("urlRegEx");
     }
 
     private static void initFetcher() {
